b6640b7b208706a48d0d59de62f2c15185ca1f1e
ramirezirvin <irvinramirez96@gmail.COM>
2021-06-23 14:38:39 -0700

    Lab 1 draft

---
+++ week1/js/init.js
@@ -0,0 +1,39 @@
+let y = 19 + 19
+            y = y + y //76???
+            const date = 23
+
+            console.log(y)
+            console.log("Hello Asian Am 191A!");
+
+
+
+
+
+
+        const map = L.map('map').setView([34.0709, -118.444], 5);
+
+        // Leaflet tile layer, i.e. the base map
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+        
+        //JavaScript let variable declaration to create a marker
+        let marker = L.marker([34.0709, -118.444]).addTo(map)
+                .bindPopup('Math Sciences 4328 aka the Technology Sandbox<br> is the lab where I work in ')
+                // .openPopup();
+                
+        fetch("map.geojson")
+            .then(response => {
+                return response.json();
+                })
+            .then(data =>{
+                // Basic Leaflet method to add GeoJSON data
+                    // the leaflet method for adding a geojson
+                    L.geoJSON(data, {
+                        pointToLayer: function (feature, latlng) {
+                            return L.circleMarker(latlng, {color: feature.properties.color});
+                        }
+                    }).bindPopup(function (layer) {
+                        return layer.feature.properties.place;
+                    }).addTo(map);
+                });
