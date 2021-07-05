3f21fb1213ec7fe60fef526776d1c6fdb21326de
ramirezirvin <irvinramirez96@gmail.com>
2021-07-05 01:10:58 -0700

    1

--- week1/js/init.js
+++ week1/js/init.js
@@ -1,37 +1,29 @@
-let y = 19 + 19
-            y = y + y //76???
-            const date = 23
+const map = L.map('map').setView([34.0709, -118.444], 8);
+const colors = ["red", "green", "blue"];
+let colorName = "";
 
-            console.log(y)
-            console.log("Hello Asian Am 191A!");
-
-
-
-
-
-
-        const map = L.map('map').setView([34.0709, -118.444], 8);
-        const colors = ["red", "green", "blue"];
-        let colorName = "";
-
-        // Leaflet tile layer, i.e. the base map
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-        
-        //JavaScript let variable declaration to create a marker
-        let marker = L.marker([34.0709, -118.444]).addTo(map)
-                .bindPopup('Math Sciences 4328 aka the Technology Sandbox<br> is the lab where I work in ')
-                .openPopup();
-                
-        fetch("citiesmap.geojson")
-            .then(response => {
-                return response.json();
-                })
-            .then(data =>{
-                // Basic Leaflet method to add GeoJSON data
-                    // the leaflet method for adding a geojson
-                    L.geoJSON(data).bindPopup(function (layer) {
-                        return layer.feature.properties.Cities-Lived;
-                    }).addTo(map);
-                });
+// Leaflet tile layer, i.e. the base map
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+      
+fetch("citiesmap.geojson")
+	.then(response => {
+		return response.json();
+		})
+    .then(data =>{
+        // Basic Leaflet method to add GeoJSON data
+            // the leaflet method for adding a geojson
+            L.geoJSON(data, {
+                pointToLayer: function (feature, latlng) {
+                    let x = Math.floor(Math.random()*colors.length)
+                    colorName = colors[x];
+                    colors.splice(x,1);
+                    return L.circleMarker(latlng, {color: colorName});
+                }
+            }).bindPopup(function (layer) {
+                return layer.feature.properties.name;
+            })
+            .addTo(map)
+            .openPopup();
+        });
