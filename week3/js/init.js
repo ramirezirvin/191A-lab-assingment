d14ac53b5b1791280ecf9fdf9c4d4ee4fbb748b0
ramirezirvin <irvinramirez96@gmail.com>
2021-07-09 14:59:16 -0700

    update!

--- week3/js/init.js
+++ week3/js/init.js
@@ -4,7 +4,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-let url = "GIVE_ME_A_URL"
+let url = "https://spreadsheets.google.com/feeds/list/1h8Yqm7tGpoBxKvxsmBvlnuBw66aDVJTO3C1Pn4yGw0w/oihmdjv/public/values?alt=json"
 fetch(url)
 	.then(response => {
 		return response.json();
