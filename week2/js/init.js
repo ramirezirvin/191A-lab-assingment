49a6e2de2a3194152258342ee2b220979d387bb0
ramirezirvin <irvinramirez96@gmail.com>
2021-07-13 23:26:47 -0700

    finsih

--- week2/js/init.js
+++ week2/js/init.js
@@ -1,4 +1,4 @@
-const myMap = L.map('mapArea').setView([34.0709, -118.444], 8);
+const myMap = L.map('mapArea').setView([34.0709, -118.444], 10);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
